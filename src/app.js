e8d715021c98cbba61688080fdbdf608c50d1be6
Klaudine <109423321+klaudine@users.noreply.github.com>
2023-10-03 05:13:51 +0000

    Finished project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let prefixes = ["the", "our", "your", "another"];
+  let adjs = ["lost", "first", "big", "beautiful"];
+  let nouns = ["child", "coder", "player", "pet", "day"];
+  let tlds = [".com", ".net", ".edu", ".org", ".io"];
+
+  for (let prefix of prefixes) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          console.log(`${prefix}${adj}${noun}${tld}`);
+        }
+      }
+    }
+  }
 };
